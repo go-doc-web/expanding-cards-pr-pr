687696d5814810e19f51386d8141a40a0ebe9d03
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-09-15 14:45:51 +0300

    progect end v 1.0

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const refs = {
+  cards: document.querySelectorAll('.card'),
+};
+
+const removeActiveClasses = () => {
+  refs.cards.forEach(card => {
+    if (card.classList.contains('active')) {
+      card.classList.remove('active');
+    }
+  });
+};
+
+refs.cards.forEach(card => {
+  card.addEventListener('click', () => {
+    removeActiveClasses();
+    card.classList.add('active');
+    console.log(card);
+  });
+});
